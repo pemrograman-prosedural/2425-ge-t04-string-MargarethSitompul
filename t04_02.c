3755814dca427fefdfcdecb949b98be6e218e15f
MargarethSitompul <margarethbungaran1@gmail.com>
2025-02-27 14:05:52 +0700

    selesai

--- t04_02.c
+++ t04_02.c
@@ -2,8 +2,17 @@
 // NIM - Name
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  
-  return 0;
+    char msk[61];
+    int i;
+
+    fgets(msk, 61, stdin);
+
+    for (i = 0; i < strlen(msk) - 1; i++) {
+        printf("%03d", msk[i]);
+    }
+
+    return 0;
 }
