ce9839ed5fda038146622ccadb2d2c9147be3b09
MargarethSitompul <margarethbungaran1@gmail.com>
2025-02-27 14:15:30 +0700

    Update t04_02.c

--- t04_02.c
+++ t04_02.c
@@ -4,15 +4,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int _argv, char **_argc) {
+int main() {
     char msk[61];
     int i;
 
     fgets(msk, 61, stdin);
+    
+    msk[strcspn(msk, "\n")] = 0;
 
-    for (i = 0; i < strlen(msk) - 1; i++) {
+    for (i = 0; i < strlen(msk); i++) {
         printf("%03d", msk[i]);
     }
 
+    printf("013\n");
+
     return 0;
 }
+
