c5783bee42adb69df9189e464cfb03e9b81086cc
MargarethSitompul <margarethbungaran1@gmail.com>
2025-02-27 13:53:29 +0700

    no1

--- t04_01.c
+++ t04_01.c
@@ -2,9 +2,42 @@
 // NIM - Name
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+int main() {
+    char str[61];
 
-  return 0;
+
+    if (fgets(str, sizeof(str), stdin) == NULL) {
+        printf("Error reading input.\n");
+        return 1;
+    }
+
+    size_t len = strlen(str);
+
+    
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+        len--; 
+    }
+
+   
+    if (len < 3 || len % 3 != 0) {
+        printf("Input tidak valid.\n");
+        return 1;
+    }
+
+    
+    for (size_t i = 0; i < len; i += 3) {
+        char asciivalue[4];  
+        strncpy(asciivalue, &str[i], 3);
+        asciivalue[3] = '\0'; 
+
+        int decimalasciivalue = atoi(asciivalue);
+        printf("%c", decimalasciivalue);
+    }
+
+    printf("\n");
+    return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 
 int main(int _argv, char **_argc) {
-  // codes
-
+  
   return 0;
 }
